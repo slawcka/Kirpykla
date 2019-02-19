3047be45c0349eaf1485523b7866bca14b1a5d85
slawcka <slaviccc@gmail.com>
2019-02-19 02:36:08 +0200

    savaites puslapiavimas

--- src/components/Paieska.js
+++ src/components/Paieska.js
@@ -2,20 +2,21 @@ import React, { Component } from 'react';
 
 class Paieska extends Component {
     state={
-        puslapis:1,
+        puslapis:0,
         puslapiuKiekis:null
 
     }
     componentWillMount() {
         this.setState({puslapiuKiekis:this.props.kalendar.length})
+        
       }
     handleClick(type){
         let current=this.state.puslapis
-        let puslapiuKiekis=this.state.puslapiuKiekis
+        let puslapiuKiekis=this.state.puslapiuKiekis-1
         type==="minus" ? current-- : type==="plus" && current++
         
-        if(current<1){
-            current=1
+        if(current<0){
+            current=0
         } else if (current>puslapiuKiekis){
             current=puslapiuKiekis
         }
@@ -25,13 +26,15 @@ class Paieska extends Component {
         })
     }
     render() {
-        const puslapis=this.state.puslapis-1
+        const puslapis=this.state.puslapis
         const data=this.props.kalendar
+		console.log('TCL: Paieska -> render -> data', data)
+        const puslapiuKiekis=this.state.puslapiuKiekis-1
         return (
             <div>
                 <h1>{data[puslapis].date}</h1>
-                <button onClick={()=>this.handleClick("minus")} >prev</button>
-                <button onClick={()=>this.handleClick("plus")}>next</button>
+                <button onClick={()=>this.handleClick("minus")} disabled={puslapis===0} >prev</button>
+                <button onClick={()=>this.handleClick("plus")} disabled={puslapis===puslapiuKiekis}>next</button>
                 {data[puslapis].laikas.map(e=>
                 <div>
                 <h1>{e.klientas}</h1>
